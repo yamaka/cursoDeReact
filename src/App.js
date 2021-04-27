ea3748400adb1f2b9b91a0668de038364bc196cb
ymk <frostmour2013@gmail.com>
2021-04-26 20:37:07 -0400

    curso de react clase 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         primera clase del curso de react.
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
